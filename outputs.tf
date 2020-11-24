a87a13d8b8ac1d1a3e13492fbfb43111d9557178
Jonatas Lopes <jlopsti@gmail.com>
2020-11-24 15:40:32 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,14 +7,6 @@ output "group_name" {
 output "group_arn" {
     value   = length(aws_iam_group.admin) > 0 ? aws_iam_group.admin.*.arn : null
 }
-output "cloudtrail" {
-    value   = [
-        "Name: ${length(aws_cloudtrail.main) > 0 ? aws_cloudtrail.main[0].name : 0}",
-        "Logging: ${length(aws_cloudtrail.main) > 0 ? aws_cloudtrail.main[0].enable_logging : 0}",
-        "Multi_Region: ${length(aws_cloudtrail.main) > 0 ? aws_cloudtrail.main[0].is_multi_region_trail : 0}",
-        "Bucket: ${length(aws_cloudtrail.main) > 0 ? aws_cloudtrail.main[0].s3_bucket_name : 0}"
-    ]
-}
 output "role_admin" {
     value   = length(aws_iam_role.enable_cross_account_admin) > 0 ? aws_iam_role.enable_cross_account_admin[0].name : null
 }
